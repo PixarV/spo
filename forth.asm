28185594cd9a15c75a796fef079aec28b6a3153a
ritt <margarita.fedotova@rtlabs.ru>
2019-06-13 22:38:18 +0000

    add set of io commands and stack min

--- forth.asm
+++ forth.asm
@@ -1,48 +1,40 @@
 global _start
-
 %include "macro.inc"
 
-%define pc r15
-%define w r14
-%define rstack r13
+%define pc r15		;next command pointer
+%define w r14		;sub commands pointer
+%define rstack r13	;return stack pointer
 
-section .text          
+section .text
 
 %include "choice.inc"
 %include "interpreter.inc"
+%include "util.asm"
 
 section .bss
 
-; return stack end-----;
-resq 1023              ;
-rstack_start: resq 1   ;
-; return stack start---;
+resq 1023				;return stack end
+rstack_start: resq 1	;return stack start
 
-input_buf: resb 1024   ; buffer to read textual words into
-user_dict:  resq 65536 ; data for words
+user_mem: resq 65536	;global data for user
 
+section .data
 
-user_mem: resq 65536   ; global data for user
+last_word: dq _lw
 
-state: resq 1          ; changes to 1 if compiling, 0 by default
-
-section .data 
-last_word: dq _lw      ; stores a pointer to the last word in dictionary
-here: dq user_dict     ; current position in words memory; 
-dp: dq user_mem        ; current global data pointer 
-
-section .rodata
-msg_no_such_word: db ": no such word", 10, 0
+dp: dq user_mem		;current global data pointer
+stack_start: dq 0	;stores a saved address of data stack
 
 section .text
-next:                  ; inner interpreter, fetches next word to execute
-    mov w, pc
-    add pc, 8
-    mov w, [w]
-    jmp [w]
 
-_start: 
-    mov rstack, rstack_start
-    mov [stack_start], rsp
-    mov pc, forth_init
+_start:
+	mov rstack, rstack_start
+	mov [stack_start], rsp
 
+	mov pc, forth_init
+
+next:
+	mov w, pc
+	add pc, 8
+	mov w, [w]
+	jmp [w]
